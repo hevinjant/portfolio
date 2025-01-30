215227736fe9258a86c56de82a23448c5774dbb7
Hevin Jant <hevin.jant@gmail.com>
2025-01-30 00:14:07 -0800

    implemented mobile styling

--- src/components/Folder.tsx
+++ src/components/Folder.tsx
@@ -2,27 +2,31 @@ import React, { useState, useEffect, useRef } from "react";
 import "../styles/Folder.css";
 
 function Folder() {
-  const tabs = ["Projects", "Experiences", "About", "Education"];
+  const tabs = ["About", "Education", "Projects", "Experiences", "Resume"];
 
   const containerRef = useRef<HTMLDivElement>(null);
   const [containerWidth, setContainerWidth] = useState<number>(0);
-  const [activeTab, setActiveTab] = useState<string | null>(null);
+  const [containerHeight, setContainerHeight] = useState<number>(0);
+  const [activeTab, setActiveTab] = useState<string | null>(tabs[0]);
 
   useEffect(() => {
     // for initial load
     if (containerRef.current) {
       setContainerWidth(containerRef.current.offsetWidth);
+      setContainerHeight(containerRef.current.offsetHeight);
     }
 
-    const getWidthListener = () => {
+    const getWidthAndHeightListener = () => {
       if (containerRef.current) {
         setContainerWidth(containerRef.current.offsetWidth);
+        setContainerHeight(containerRef.current.offsetHeight);
       }
     };
 
-    window.addEventListener("resize", getWidthListener);
+    window.addEventListener("resize", getWidthAndHeightListener);
 
-    return () => window.removeEventListener("resize", getWidthListener);
+    return () =>
+      window.removeEventListener("resize", getWidthAndHeightListener);
   }, []);
 
   return (
@@ -30,11 +34,14 @@ function Folder() {
       <FolderTabs
         tabs={tabs}
         containerWidth={containerWidth}
+        containerHeight={containerHeight}
         activeTab={activeTab}
         setActiveTab={setActiveTab}
       />
       <div ref={containerRef} className="folder-container">
-        <div className="folder-content">{containerWidth}</div>
+        <div className="folder-content">
+          {containerWidth}x{containerHeight}
+        </div>
       </div>
     </div>
   );
@@ -43,6 +50,7 @@ function Folder() {
 type FolderTabsProps = {
   tabs: string[];
   containerWidth: number;
+  containerHeight: number;
   activeTab: string | null;
   setActiveTab: React.Dispatch<React.SetStateAction<string | null>>;
 };
@@ -50,6 +58,7 @@ type FolderTabsProps = {
 function FolderTabs({
   tabs,
   containerWidth,
+  containerHeight,
   activeTab,
   setActiveTab,
 }: FolderTabsProps) {
@@ -59,22 +68,43 @@ function FolderTabs({
     containerWidth / numberOfTabs
   );
 
+  const [tabHeight, setTabHeight] = useState<number>(
+    containerHeight / numberOfTabs
+  );
+
   useEffect(() => {
     setTabWidth(containerWidth / numberOfTabs);
   }, [containerWidth]);
 
+  useEffect(() => {
+    setTabHeight(containerHeight / numberOfTabs);
+  }, [containerHeight]);
+
   return (
-    <div className="tabs-container">
-      {tabs.map((tab) => (
-        <button
-          onClick={() => setActiveTab(tab)}
-          className={`folder-tab ${activeTab === tab ? "active" : ""}`}
-          style={{ width: `${tabWidth - 2}px` }}
-        >
-          <div className="folder-tab-title">{tab}</div>
-        </button>
-      ))}
-    </div>
+    <>
+      <div className="tabs-container">
+        {tabs.map((tab) => (
+          <button
+            onClick={() => setActiveTab(tab)}
+            className={`folder-tab ${activeTab === tab ? "active" : ""}`}
+            style={{ width: `${tabWidth - 2}px` }}
+          >
+            <div className="folder-tab-title">{tab}</div>
+          </button>
+        ))}
+      </div>
+      <div className="tabs-container-mobile">
+        {tabs.map((tab) => (
+          <button
+            onClick={() => setActiveTab(tab)}
+            className={`folder-tab ${activeTab === tab ? "active" : ""}`}
+            style={{ height: `${tabHeight - 2}px` }}
+          >
+            <div className="folder-tab-title">{tab}</div>
+          </button>
+        ))}
+      </div>
+    </>
   );
 }
 
